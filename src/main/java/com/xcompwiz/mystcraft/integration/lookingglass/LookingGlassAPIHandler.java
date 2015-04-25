ae4e10e2b1afa53ef8810bb455077a273a79eb60
XCompWiz <xcompwiz@gmail.com>
2015-04-25 23:24:36 +0300

    Adds log message for LookingGlass API handler entry

--- src/main/java/com/xcompwiz/mystcraft/integration/lookingglass/LookingGlassAPIHandler.java
+++ src/main/java/com/xcompwiz/mystcraft/integration/lookingglass/LookingGlassAPIHandler.java
@@ -3,10 +3,12 @@ package com.xcompwiz.mystcraft.integration.lookingglass;
 import com.xcompwiz.lookingglass.api.APIInstanceProvider;
 import com.xcompwiz.mystcraft.api.impl.InternalAPI;
 import com.xcompwiz.mystcraft.client.shaders.ShaderUtils;
+import com.xcompwiz.mystcraft.logging.LoggerUtils;
 
 public class LookingGlassAPIHandler {
 
 	public static void register(APIInstanceProvider provider) {
+		LoggerUtils.info("LookingGlass API Provider Received");
 		ShaderUtils.registerShaders();
 		InternalAPI.render.registerRenderEffect(new DynamicLinkPanelRenderer(/*api*/));
 	}
