86721ce7245a88232f92bdd251f8b2d538b2e5be
XCompWiz <xcompwiz@gmail.com>
2015-04-25 23:16:21 +0300

    Puts the LookingGlass integration back in place (disabled during API revision)

--- src/main/java/com/xcompwiz/mystcraft/integration/lookingglass/LookingGlassAPIHandler.java
+++ src/main/java/com/xcompwiz/mystcraft/integration/lookingglass/LookingGlassAPIHandler.java
@@ -1,13 +1,13 @@
-//package com.xcompwiz.mystcraft.integration.lookingglass;
-//
-//import com.xcompwiz.lookingglass.api.APIInstanceProvider;
-//import com.xcompwiz.mystcraft.client.shaders.ShaderUtils;
-//import com.xcompwiz.mystcraft.core.InternalAPI;
-//
-//public class LookingGlassAPIHandler {
-//
-//	public static void register(APIInstanceProvider provider) {
-//		ShaderUtils.registerShaders();
-//		InternalAPI.render.registerRenderEffect(new DynamicLinkPanelRenderer(/*api*/));
-//	}
-//}
+package com.xcompwiz.mystcraft.integration.lookingglass;
+
+import com.xcompwiz.lookingglass.api.APIInstanceProvider;
+import com.xcompwiz.mystcraft.api.impl.InternalAPI;
+import com.xcompwiz.mystcraft.client.shaders.ShaderUtils;
+
+public class LookingGlassAPIHandler {
+
+	public static void register(APIInstanceProvider provider) {
+		ShaderUtils.registerShaders();
+		InternalAPI.render.registerRenderEffect(new DynamicLinkPanelRenderer(/*api*/));
+	}
+}
