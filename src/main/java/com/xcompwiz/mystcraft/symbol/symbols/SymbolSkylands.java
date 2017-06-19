615646172b1e3502c24abf62c46f906f71fb2eaf
XCompWiz <xcompwiz@gmail.com>
2017-06-19 19:52:48 +0300

    Improvements to floating islands (buffer falling blocks; better noise gen; fix fluids not being trimmed)

--- src/main/java/com/xcompwiz/mystcraft/symbol/symbols/SymbolSkylands.java
+++ src/main/java/com/xcompwiz/mystcraft/symbol/symbols/SymbolSkylands.java
@@ -6,7 +6,7 @@ import com.xcompwiz.mystcraft.api.world.AgeDirector;
 import com.xcompwiz.mystcraft.api.world.logic.ITerrainAlteration;
 import com.xcompwiz.mystcraft.symbol.SymbolBase;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockFalling;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -40,31 +40,44 @@ public class SymbolSkylands extends SymbolBase {
 
 		@Override
 		public void alterTerrain(World worldObj, int chunkX, int chunkZ, ChunkPrimer primer) {
-			skyNoise = noiseGen.generateNoiseOctaves(skyNoise, chunkX * 16, chunkZ * 16, 16, 16, 16, 1, factor * 64D, factor * 32D, factor * 64D);
+			skyNoise = noiseGen.generateNoiseOctaves(skyNoise, chunkX * 16, chunkZ * 16, 16, 16, factor * 32D, factor * 32D, 1);
 			int layers = 256;
-			for (int y = 0; y < layers; ++y) {
-				for (int z = 0; z < 16; ++z) {
-					for (int x = 0; x < 16; ++x) {
-						int height = (76) + (int) (skyNoise[z | x << 4]);
-						int coords = y << 8 | z << 4 | x;
-
-						if (primer.getBlockState(x, y, z).getBlock().equals(Blocks.WATER) && !isSupported(x, y, z, height, primer)) {
-							primer.setBlockState(x, y, z, Blocks.AIR.getDefaultState());
-						}
+			for (int z = 0; z < 16; ++z) {
+				for (int x = 0; x < 16; ++x) {
+					int height = calcSkyCutoff(x, z, skyNoise);
+					for (int y = 0; y < layers; ++y) {
 						if (y <= height) {
 							primer.setBlockState(x, y, z, Blocks.AIR.getDefaultState());
+						} else {
+							IBlockState blockstate = primer.getBlockState(x, y, z);
+							if (blockstate.getMaterial().isLiquid()) { // Detect liquids
+								if (!isSupported(x, y, z, height, primer)) { // remove poorly supported liquids
+									primer.setBlockState(x, y, z, Blocks.AIR.getDefaultState());
+								}
+							} else if (blockstate.getBlock() instanceof BlockFalling) {
+								if (!isSupported(x, y, z, height, primer)) { // Solidify poorly supported falling blocks
+									primer.setBlockState(x, y, z, Blocks.STONE.getDefaultState());
+								}
+							}
 						}
 					}
 				}
 			}
 		}
 
+		private int calcSkyCutoff(int x, int z, double[] skyNoise) {
+			return (76) + (int) (skyNoise[(x) << 5 | (z)]);
+		}
+
 		private boolean isSupported(int x, int y, int z, int sky, ChunkPrimer primer) {
 			if (y < 1) return false;
+			if (y - 1 <= sky) return false;
 			IBlockState block = primer.getBlockState(x, y - 1, z);
-			if (block.getBlock().equals(Blocks.AIR)) return false;
-			if (block.getBlock().equals(Blocks.WATER)) return false;
-			return false;
+			if (block.getBlock() == Blocks.COBBLESTONE) return false;
+			if (block.getBlock() == Blocks.AIR) return false;
+			if (block.getMaterial().isLiquid()) return false;
+			if (!block.getMaterial().isSolid()) return false;
+			return true;
 		}
 	}
 }
