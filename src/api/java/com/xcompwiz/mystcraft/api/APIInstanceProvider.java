67f525927f3d4069c47aab5aa8c97d0a5d5da357
XCompWiz <xcompwiz@gmail.com>
2018-05-21 00:17:02 +0300

    Improve API usage doc

--- src/api/java/com/xcompwiz/mystcraft/api/APIInstanceProvider.java
+++ src/api/java/com/xcompwiz/mystcraft/api/APIInstanceProvider.java
@@ -5,33 +5,31 @@ import com.xcompwiz.mystcraft.api.exception.APIVersionRemoved;
 import com.xcompwiz.mystcraft.api.exception.APIVersionUndefined;
 
 /**
- * The purpose of this interface is simply to provide instances of the API interfaces.
- * It is possible to request an instance via the static method, but you should first make sure Mystcraft is loaded.
- * This interface supports providing multiple versions of an API, so you can request ex. 'someAPI-1' and always get the same interface,
- * enabling the API to move forward without breaking compatibility.
- * The APIInstanceProvider instance returned, as well as any API interface instances created by it, will belong to the mod which requested the provider.
- * To see what API interfaces are available, see the hook package.
+ * The purpose of this interface is simply to provide instances of the API interfaces. It is possible to request an instance via the static method, but you
+ * should first make sure Mystcraft is loaded. This interface supports providing multiple versions of an API, so you can request ex. 'someAPI-1' and always get
+ * the same interface, enabling the API to move forward without breaking compatibility. The APIInstanceProvider instance returned, as well as any API interface
+ * instances created by it, will belong to the mod which requested the provider. To see what API interfaces are available, see the hook package.
  * @author xcompwiz
  */
 public interface APIInstanceProvider {
 
 	/**
-	 * Returns a constructed version of the requested API. If the API requested doesn't exist then an exception will be thrown.
-	 * Be wary when attempting to cast the returned instance, as, if you try using an interface not included in the class path,
-	 * you may get missing definition crashes.
-	 * It is wiser to, after verifying success, pass the object to another function which can handle the casting.
+	 * Returns a constructed version of the requested API. If the API requested doesn't exist then an exception will be thrown. Be wary when attempting to cast
+	 * the returned instance, as, if you try using an interface not included in the class path, you may get missing definition crashes. It is wiser to, after
+	 * verifying success, pass the object to another function which can handle the casting.
 	 * @param api The name of the API and version desired, formatted as ex. 'symbol-1'.
-	 * @return The requested API instance as an Object.  If you get an object, it is guaranteed to be of the requested API and version. 
+	 * @return The requested API instance as an Object. If you get an object, it is guaranteed to be of the requested API and version.
 	 */
 	public Object getAPIInstance(String api) throws APIUndefined, APIVersionUndefined, APIVersionRemoved;
 
 	public static APIInstanceProvider getProviderInstance() {
 		return com.xcompwiz.mystcraft.api.impl.InternalAPI.getAPIProviderInstance();
 	}
-	
+
+//@formatter:off
 	/*	Example Usage
  	public static void doMystcraftIntegration() {
- 		if (!Loader.isModLoaded("Mystcraft"))
+ 		if (!Loader.isModLoaded("Mystcraft")) //Note, if the Mystcraft API package isn't included in your packaged code, you'll need to make this check before calling this function
  			return;
 
  		APIInstanceProvider provider = APIInstanceProvider.getProviderInstance();
@@ -52,7 +50,7 @@ public interface APIInstanceProvider {
 
 	// alternate way, should you want to ensure all of the integration can occur properly
  	public static void doMoreComplexMystcraftIntegration() {
- 		if (!Loader.isModLoaded("Mystcraft"))
+ 		if (!Loader.isModLoaded("Mystcraft")) //Note, if the Mystcraft API package isn't included in your packaged code, you'll need to make this check before calling this function
  			return;
 
  		APIInstanceProvider provider = APIInstanceProvider.getProviderInstance();
@@ -67,4 +65,5 @@ public interface APIInstanceProvider {
  		doActualIntegration(api1, api2);
 	}
 	*/
+//@formatter:on
 }
