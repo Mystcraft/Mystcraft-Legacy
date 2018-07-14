5ca0e5d916afaa90175d8b2d3dd8c9fb6c53b31a
XCompWiz <xcompwiz@gmail.com>
2018-07-15 02:24:17 +0300

    Huge Tree code cleanup and height tweaking

--- src/main/java/com/xcompwiz/mystcraft/world/gen/feature/WorldGenMystBigTree.java
+++ src/main/java/com/xcompwiz/mystcraft/world/gen/feature/WorldGenMystBigTree.java
@@ -14,56 +14,41 @@ public class WorldGenMystBigTree extends MapGenAdvanced {
 	static final int coordMaximums[] = { 16, 255, 16 };
 
 	World worldObj;
-	int basePos[] = { 0, 0, 0 };
-	int trueBase[] = { 0, 0, 0 };
-	int heightLimit;
-	int height;
-	double heightAttenuation;
-	double scaleWidth;
-	double leafDensity;
-	int trunkSize;
-	int heightLimitLimit;
-	int leafDistanceLimit;
+	int blobPos[] = { 0, 0, 0 };
+	int rootPos[] = { 0, 0, 0 };
+	int blobHeight;
 	int leafNodes[][];
-	static private int max_height = 50;
+
+	private static double scaleWidth = 0.2D;
+	private static int maxY = 180;
+	private static int minY = 128;
+	private static int leafDistanceLimit = 3;
+	private static int trunkSize = 2;
 
 	public WorldGenMystBigTree(long seed) {
 		super(seed, Blocks.LOG.getDefaultState());
 
-		heightLimit = 0;
-		heightAttenuation = 0.9D;
-		scaleWidth = 0.2D;
-		leafDensity = 0.8D;
-		trunkSize = 2;
-		heightLimitLimit = max_height;
-		leafDistanceLimit = 3;
+		blobHeight = 0;
 	}
 
 	void generateLeafNodeList(ChunkPrimer primer) {
-		height = (int) (heightLimit * heightAttenuation);
-		if (height >= heightLimit) {
-			height = heightLimit - 1;
-		}
-		if (height < max_height / 2) {
-			return;
-		}
-		int i = 4;// (int)(1.0D + Math.pow((leafDensity * (double)heightLimit) / 13D, 2D));
+		int i = 4;
 		if (i < 1) {
 			i = 1;
 		}
-		int ai[][] = new int[i * heightLimit][4];
-		int j = (basePos[1] + heightLimit) - leafDistanceLimit;
+		int ai[][] = new int[i * blobHeight][4];
+		int j = (blobPos[1] + blobHeight) - leafDistanceLimit;
 		int k = 1;
-		int l = basePos[1] + height;
-		int i1 = j - basePos[1];
-		ai[0][0] = basePos[0];
+		int l = blobPos[1] + blobHeight;
+		int i1 = j - blobPos[1];
+		ai[0][0] = blobPos[0];
 		ai[0][1] = j;
-		ai[0][2] = basePos[2];
+		ai[0][2] = blobPos[2];
 		ai[0][3] = l;
 		--j;
 		int ai1[] = { 0, 0, 0 };
 		int ai2[] = { 0, 0, 0 };
-		int ai3[] = { basePos[0], 0, basePos[2] };
+		int ai3[] = { blobPos[0], 0, blobPos[2] };
 		double d = 0.5D;
 		while (i1 >= 0) {
 			float f = layerSize(i1);
@@ -74,25 +59,20 @@ public class WorldGenMystBigTree extends MapGenAdvanced {
 				for (int j1 = 0; j1 < i; ++j1) {
 					double d1 = scaleWidth * (f * (rand.nextFloat() + 3.0D)); // Limb length (position of node)
 					double d2 = rand.nextFloat() * 2D * 3.1415899999999999D; // direction
-					int k1 = MathHelper.floor(d1 * Math.sin(d2) + basePos[0] + d);
-					int l1 = MathHelper.floor(d1 * Math.cos(d2) + basePos[2] + d);
+					int k1 = MathHelper.floor(d1 * Math.sin(d2) + blobPos[0] + d);
+					int l1 = MathHelper.floor(d1 * Math.cos(d2) + blobPos[2] + d);
 					ai1[0] = k1;
 					ai1[1] = j;
 					ai1[2] = l1;
 					ai2[0] = k1;
 					ai2[1] = j + leafDistanceLimit;
 					ai2[2] = l1;
-					if (checkBlockLine(ai1, ai2) != -1) {
-						continue;
-					}
 					ai3[1] = j - 2;
-					if (checkBlockLine(ai3, ai1) == -1) {
-						ai[k][0] = k1;
-						ai[k][1] = j;
-						ai[k][2] = l1;
-						ai[k][3] = ai3[1];
-						++k;
-					}
+					ai[k][0] = k1;
+					ai[k][1] = j;
+					ai[k][2] = l1;
+					ai[k][3] = ai3[1];
+					++k;
 				}
 
 				--j;
@@ -131,11 +111,11 @@ public class WorldGenMystBigTree extends MapGenAdvanced {
 	}
 
 	float layerSize(int i) {
-		if (i < heightLimit * 0.75D) {
+		if (i < blobHeight * 0.75D) {
 			return -1.618F;
 		}
-		float f = heightLimit * 0.5F;
-		float f1 = heightLimit * 0.5F - i;
+		float f = blobHeight * 0.5F;
+		float f1 = blobHeight * 0.5F - i;
 		float f2;
 		if (f1 == 0.0F) {
 			f2 = f;
@@ -225,14 +205,14 @@ public class WorldGenMystBigTree extends MapGenAdvanced {
 	}
 
 	boolean leafNodeNeedsBase(int i) {
-		return i >= heightLimit * 0.20000000000000001D;
+		return i >= blobHeight * 0.20000000000000001D;
 	}
 
 	void generateTrunk(ChunkPrimer primer) {
-		int i = trueBase[0];
-		int j = trueBase[1];
-		int k = basePos[1] + height;
-		int l = trueBase[2];
+		int i = rootPos[0];
+		int j = rootPos[1];
+		int k = blobPos[1] + blobHeight - 5;
+		int l = rootPos[2];
 		int ai[] = { i, j, l };
 		int ai1[] = { i, k, l };
 		placeBlockLine(primer, ai, ai1, Blocks.LOG.getDefaultState());
@@ -250,12 +230,12 @@ public class WorldGenMystBigTree extends MapGenAdvanced {
 	}
 
 	void generateRoots(ChunkPrimer primer) {
-		int i = trueBase[0];
-		int j = trueBase[1];
-		int k = trueBase[2];
+		int i = rootPos[0];
+		int j = rootPos[1];
+		int k = rootPos[2];
 		int ai[] = { i, j + 1, k };
 		int ai1[] = { i, j, k };
-		int range = height >> 2;
+		int range = blobHeight >> 2;
 		for (int c = 0; c < range; ++c) {
 			ai[0] = i + c % 2;
 			ai[2] = k + (c > 2 ? 1 : 0);
@@ -269,98 +249,16 @@ public class WorldGenMystBigTree extends MapGenAdvanced {
 	void generateLeafNodeBases(ChunkPrimer primer) {
 		int i = 0;
 		int j = leafNodes.length;
-		int ai[] = { basePos[0], basePos[1], basePos[2] };
+		int ai[] = { blobPos[0], blobPos[1], blobPos[2] };
 		for (; i < j; i++) {
 			int ai1[] = leafNodes[i];
 			int ai2[] = { ai1[0], ai1[1], ai1[2] };
 			ai[1] = ai1[3];
-			int k = ai[1] - basePos[1];
+			int k = ai[1] - blobPos[1];
 			if (leafNodeNeedsBase(k)) {
 				placeBlockLine(primer, ai, ai2, Blocks.LOG.getDefaultState());
 			}
 		}
-	}
-
-	int checkBlockLine(int ai[], int ai1[]) {
-		return -1;
-		// int ai2[] =
-		// {
-		// 0, 0, 0
-		// };
-		// byte byte0 = 0;
-		// int i = 0;
-		// for (; byte0 < 3; ++byte0) {
-		// ai2[byte0] = ai1[byte0] - ai[byte0];
-		// if (Math.abs(ai2[byte0]) > Math.abs(ai2[i])) {
-		// i = byte0;
-		// }
-		// }
-		//
-		// if (ai2[i] == 0) {
-		// return -1;
-		// }
-		// byte byte1 = otherCoordPairs[i];
-		// byte byte2 = otherCoordPairs[i + 3];
-		// byte byte3;
-		// if (ai2[i] > 0) {
-		// byte3 = 1;
-		// } else {
-		// byte3 = -1;
-		// }
-		// double d = (double)ai2[byte1] / (double)ai2[i];
-		// double d1 = (double)ai2[byte2] / (double)ai2[i];
-		// int ai3[] =
-		// {
-		// 0, 0, 0
-		// };
-		// int j = 0;
-		// int k = ai2[i] + byte3;
-		// do
-		// {
-		// if (j == k)
-		// {
-		// break;
-		// }
-		// ai3[i] = ai[i] + j;
-		// ai3[byte1] = MathHelper.floor((double)ai[byte1] + (double)j * d);
-		// ai3[byte2] = MathHelper.floor((double)ai[byte2] + (double)j * d1);
-		// int l = getBlockId(ai3[0], ai3[1], ai3[2]);
-		// if (l != 0 && l != 18) {
-		// break;
-		// }
-		// j += byte3;
-		// }
-		// while (true);
-		// if (j == k)
-		// {
-		// return -1;
-		// }
-		// else
-		// {
-		// return Math.abs(j);
-		// }
-	}
-
-	boolean validTreeLocation(ChunkPrimer primer) {
-		int ai[] = { basePos[0], basePos[1], basePos[2] };
-		IBlockState block = primer.getBlockState(basePos[0], basePos[1] - 1, basePos[2]);
-		while (block == Blocks.WATER || block == Blocks.FLOWING_WATER) {
-			--basePos[1];
-			block = primer.getBlockState(basePos[0], basePos[1] - 1, basePos[2]);
-		}
-		if (block.getBlock() != Blocks.DIRT && block.getBlock() != Blocks.GRASS) {
-			return false;
-		}
-		int ai1[] = { basePos[0], (basePos[1] + heightLimit) - 1, basePos[2] };
-		int j = checkBlockLine(ai, ai1);
-		if (j == -1) {
-			return true;
-		}
-		if (j < max_height / 2) {
-			return false;
-		}
-		heightLimit = j;
-		return true;
 	}
 
 	/**
@@ -372,20 +270,13 @@ public class WorldGenMystBigTree extends MapGenAdvanced {
 		int pY = rand.nextInt(16);
 		int pZ = z * 16 + rand.nextInt(16) - chunkZ * 16;
 
-		trueBase[0] = basePos[0] = pX;
-		basePos[1] = pY;
-		trueBase[2] = basePos[2] = pZ;
-		if (heightLimit == 0) {
-			heightLimit = 20 + rand.nextInt(heightLimitLimit);
-		}
-//		if (!validTreeLocation(primer)) {
-//			return;
-//		}
-		trueBase[1] = basePos[1];
-		basePos[1] = Math.max(trueBase[1], 128 - heightLimit - rand.nextInt(16));
+		rootPos[0] = blobPos[0] = pX;
+		rootPos[1] = blobPos[1] = pY;
+		rootPos[2] = blobPos[2] = pZ;
+
+		blobPos[1] = rand.nextInt(maxY - minY + 1) + minY;
+		blobHeight = rand.nextInt(30) + 40;
 		generateLeafNodeList(primer);
-		if (height < max_height / 2)
-			return;
 		generateLeaves(primer);
 		generateTrunk(primer);
 		generateRoots(primer);
